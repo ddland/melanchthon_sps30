8ada7c1a6741552a318ee704699bd13d5de9f8e7
Derek Land <derek@ddmr.nl>
2025-12-02 16:43:46 +0100

    nieuwe datasets

--- plotten/plot_boxplot.py
+++ plotten/plot_boxplot.py
@@ -3,36 +3,42 @@ import matplotlib.pyplot as plt
 import os.path 
 import glob
 
-def per_dag(base_dir, col=1):
+def per_file(base_dir, col=1):
     """ Lees verscihllende datasets in, waarbij de meetdata uit de filenaam gehaald wordt
+    De eerste column is een tijdstamp, daarna zijn er colummen met data.
     
     argumenten:
        base_dir: directory waar files staan
-        col: column nummer waar de data staat
+       col: column nummer waar de data staat
     """
     alldata = []
     times = []
     for fh in sorted(glob.glob(os.path.join(base_dir, '*.csv'))):
         data = np.loadtxt(fh, delimiter=';', skiprows=1)
         alldata.append(data[:,col])
-        # maand staat op positie 20:22 en dag op 22:24 van de filenaam + directory
-        times.append(fh[20:22] +'-' + fh[22:24])
+        times.append(np.mean(data[:,0])) # timestamps in col 0
+    times = np.array(times).astype('datetime64[s]')
     return times, alldata
 
 
 # maak tijd-as en datasets aan aan de hand van verschillende files in directory
-mean_time, datasets = per_dag('melanchthon_001')
+mean_time, datasets = per_file('data', col=2)
 
 # creeer een figuur
 fig, ax = plt.subplots(1)
-ax.boxplot(datasets)
+ax.boxplot(datasets, tick_labels=mean_time)
 
 # teken een grid, label de assen
 ax.grid()
-ax.set_xlabel('tijd')
+ax.set_xlabel('activiteit')
 ax.set_ylabel('PM2.5 [ug/m]')
-ax.set_xticklabels([x for x in mean_time], rotation=40)
 
+# labels met een gemiddelde tijd
+#ax.set_xticklabels([x.item().strftime('%m-%d')  # maand-dag
+#                    for x in mean_time], rotation=40)
+# labels met tekst passend bij files
+ax.set_xticklabels(['achtergrond binnen', 'kaars binnen','buiten'], rotation=40)
+#fig.autofmt_xdate()
 plt.tight_layout()
 # sla de data op
 plt.savefig('figuur.pdf')
--- plotten/plot_simple.py
+++ plotten/plot_simple.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # lees data
-data = np.loadtxt('melanchthon_001/20241123.csv',
+data = np.loadtxt('data/03_buiten.csv',
                   delimiter=';',
                   skiprows=1)
 
@@ -10,12 +10,16 @@ data = np.loadtxt('melanchthon_001/20241123.csv',
 t = np.asarray(data[:,0], dtype='datetime64[s]')
 
 # plot de eerste column uitgezet tegen de tijd
-plt.plot(t, data[:,1])
+plt.plot(t, data[:,1], label='PM 1.0')
+plt.plot(t, data[:,2], label='PM 2.5')
+plt.plot(t, data[:,3], label='PM 4.0')
+plt.plot(t, data[:,4], label='PM 10')
 
 # teken grid, assen
 plt.grid()
 plt.xlabel('tijd')
-plt.ylabel('PM2.5 [ug/m]')
+plt.ylabel('fijnstof [ug/m]')
+plt.legend(loc=0)
 plt.gcf().autofmt_xdate()
 
 # sla het figuur op
